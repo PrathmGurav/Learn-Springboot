5034f9f0c80e007111d541cf319bd2d302679ae7
a <a@a-Vostro-3670>
2025-05-08 18:13:58 +0530

    dc

--- Learn-SpringBoot/src/main/java/com/LearnSpringBoot/Learn_SpringBoot/LearnSpringBootApplication.java
+++ Learn-SpringBoot/src/main/java/com/LearnSpringBoot/Learn_SpringBoot/LearnSpringBootApplication.java
@@ -9,7 +9,7 @@ public class LearnSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringBootApplication.class, args);
 		
-		System.out.println("jndivoervo");
+		System.out.println("Hello");
 	}
 
 }
